adc365e072c69b4cd14170d076324dd1c7df4277
Nirav Patel <email@example.com>
2019-05-10 14:48:09 -0400

    added svg image

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -51,8 +51,7 @@ class Landing extends React.PureComponent {
             Start
           </BaseLink> */}
           <div className="gamebutton">
-            <img src={require('../../../src/assets/svg/plain-button.svg')} />
-            <img src="https://files.slack.com/files-pri/T026JTKEW-FJ8UYC16W/play-button.png" />
+            <img src={require('../../../src/assets/svg/plain-button.svg')} alt="Play" />
           </div>
         </div>
       </section>
