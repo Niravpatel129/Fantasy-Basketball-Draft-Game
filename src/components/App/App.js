b6398078b3858049530f358ddf938f35ac14e649
Nirav Patel <email@example.com>
2019-05-10 13:58:12 -0400

    Cleared Landing Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import debounce from 'lodash.debounce';
 import { Transition } from 'react-transition-group';
-import { Footer, HamburgerMenu, MainTopNav, PageOverlay } from '@jam3/react-ui';
+// import { Footer, HamburgerMenu, MainTopNav, PageOverlay } from '@jam3/react-ui';
 import { device } from '@jam3/detect';
 import checkProps from '@jam3/react-check-extra-props';
 import 'default-passive-events';
@@ -16,9 +16,9 @@ import { setPreviousRoute, setWindowSize, setLayout, batchActions } from '../../
 import { setIsMobileMenuOpen } from '../../redux/modules/main-nav';
 
 import settings from '../../data/settings';
-import mainNavData from '../../data/main-nav';
-import hamburgerNavData from '../../data/hamburger-menu';
-import footerData from '../../data/footer';
+// import mainNavData from '../../data/main-nav';
+// import hamburgerNavData from '../../data/hamburger-menu';
+// import footerData from '../../data/footer';
 import rotateScreenData from '../../data/rotate-screen';
 import layout from '../../util/layout';
 
@@ -71,7 +71,7 @@ class App extends React.PureComponent {
             /> */}
             {!this.props.layout.large && (
               <Fragment>
-                <PageOverlay
+                {/* <PageOverlay
                   isShowing={this.props.isMobileMenuOpen}
                   onClick={() => this.props.setIsMobileMenuOpen(false)}
                 />
@@ -79,7 +79,7 @@ class App extends React.PureComponent {
                   {...hamburgerNavData}
                   isMobileMenuOpen={this.props.isMobileMenuOpen}
                   setIsMobileMenuOpen={this.props.setIsMobileMenuOpen}
-                />
+                /> */}
               </Fragment>
             )}
             <Pages />
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -46,41 +46,12 @@ class Landing extends React.PureComponent {
 
   render() {
     return (
-      <section className={classnames('Landing', this.props.className)} ref={el => (this.container = el)}>
-        <header className="Landing-header">
-          <h1 className="Landing-title">Tournament Generator</h1>
-        </header>
-        <section className="Landing-intro">
-          <h2>What is it?</h2>
-          <div>
-            It's an scaffolding result of the Jam3 Generator. This generator includes many of the modern stack we use
-            internally on Jam3 and implement many of the best practices are in our projects
-          </div>
-        </section>
-        <section className="Landing-source">
-          <h2>Source code</h2>
-          <div>
-            The source code of the generator is open source, you can check it out on{' '}
-            <BaseLink link="https://github.com/Jam3/nyg-jam3">https://github.com/Jam3/nyg-jam3</BaseLink>
-          </div>
-        </section>
-        <section className="Landing-resources">
-          <h2>Resources</h2>
-          <div className="container">
-            <BaseLink className="resource" link="https://generator.jam3.net/components">
-              Components
-            </BaseLink>
-            <BaseLink className="resource" link="https://generator.jam3.net/styleguide">
-              Styleguide
-            </BaseLink>
-            <BaseLink className="resource" link="https://generator.jam3.net/performance">
-              Performance
-            </BaseLink>
-            <BaseLink className="resource" link="https://generator.jam3.net/bundle">
-              Bundle
-            </BaseLink>
-          </div>
-        </section>
+      <section className="Landing-resources">
+        <div className="container">
+          <BaseLink className="resource" link="https://generator.jam3.net/components">
+            Start
+          </BaseLink>
+        </div>
       </section>
     );
   }
