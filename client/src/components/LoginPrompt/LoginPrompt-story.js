e4ca871618d22b1ad48ffb28546997a28354e0f8
adam-vanegdom <adam.vanegdom@jam3.com>
2019-05-13 12:35:46 -0400

    added login prompt component

---
+++ client/src/components/LoginPrompt/LoginPrompt-story.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Component from './LoginPrompt';
+
+storiesOf('LoginPrompt', module).add('Default', () => <Component />);
---
+++ client/src/components/LoginPrompt/LoginPrompt.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import './LoginPrompt.scss';
+
+import checkProps from '../../util/check-props';
+
+class LoginPrompt extends React.PureComponent {
+  state = {};
+
+  componentDidMount() {}
+
+  componentDidUpdate(prevProps, prevState) {}
+
+  render() {
+    return <div className={classnames(`LoginPrompt`, this.props.className)}>LoginPrompt component</div>;
+  }
+}
+
+LoginPrompt.propTypes = checkProps({
+  className: PropTypes.string
+});
+
+LoginPrompt.defaultProps = {};
+
+export default LoginPrompt;
