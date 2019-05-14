06fac04582594e73dfe3efc4975612b2ea78052a
Nirav Patel <email@example.com>
2019-05-14 11:11:52 -0400

    Server has database now

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -5,13 +5,13 @@ import { connect } from 'react-redux';
 import wait from '@jam3/wait';
 import checkProps from '@jam3/react-check-extra-props';
 import './Landing.scss';
+import axios from 'axios';
 import Transition from '../PagesTransitionWrapper';
 import { setLandingLoaded } from '../../redux/modules/landing';
 import animate from '../../util/gsap-animate';
 import Titletext from '../../components/Titletext/Titletext';
 import Arrow from '../../components/Arrow/Arrow';
 import GameButton from '../../components/GameButton/GameButton';
-
 class Landing extends React.PureComponent {
   componentDidMount() {
     animate.set(this.container, { autoAlpha: 0 });
@@ -20,8 +20,19 @@ class Landing extends React.PureComponent {
       // await for data to be loaded here e.g. via fetch
       this.props.setLandingLoaded(true);
     }
+    axios
+      .post('/vote', {
+        user: 'Fred',
+        teamPicked: 'TeamB'
+      })
+      .then(function(response) {
+        // console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
-
+  x;
   onAppear = () => {
     this.animateIn();
   };
