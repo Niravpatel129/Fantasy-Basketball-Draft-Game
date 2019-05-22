3442a3317b6e5ee4f5b78f90696bfcdf9891de89
adam-vanegdom <adam.vanegdom@jam3.com>
2019-05-22 10:33:03 -0400

    merge

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
-var mongoose = require("mongoose");
-const axios = require("axios");
+var mongoose = require('mongoose');
+const axios = require('axios');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Mongoose
-mongoose.connect(
-  "mongodb://dbadmin:dbadmin1@ds155076.mlab.com:55076/tournament",
-  { useNewUrlParser: true }
-);
+mongoose.connect('mongodb://dbadmin:dbadmin1@ds155076.mlab.com:55076/tournament', { useNewUrlParser: true });
 
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("connection success to database!");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connection success to database!');
 
   // Person1.save();
 });
@@ -37,12 +34,12 @@ var scoreData = new mongoose.Schema({
   score: Number
 });
 
-var Scores = mongoose.model("Scores", scoreData);
+var Scores = mongoose.model('Scores', scoreData);
 
-var User = mongoose.model("User", personData);
+var User = mongoose.model('User', personData);
 
 // Client API Calls
-app.post("/logPicks", (req, res) => {
+app.post('/logPicks', (req, res) => {
   var picks = req.body.logPicks.picks.map(pick => {
     return {
       gameId: pick.gameId,
@@ -63,11 +60,11 @@ app.post("/logPicks", (req, res) => {
 
   Score1.save();
   Person1.save();
-  console.log("saved");
+  console.log('saved');
 });
 
 //leaderboards
-app.get("/leaderboards", (req, res) => {
+app.get('/leaderboards', (req, res) => {
   var leaderboards = [];
   User.find({}, function(err, docs) {
     docs.map(data => {
@@ -84,107 +81,92 @@ app.get("/leaderboards", (req, res) => {
   console.log(leaderboards);
 });
 
-app.get("/checkIfAlreadyPickedToday", (req, res) => {
-  User.find(
-    { username: req.query.name, Date: req.query.date },
-    (err, response) => {
-      if (response.length > 0) {
-        res.send("found");
-      } else {
-        res.send("notfound");
-      }
+app.get('/checkIfAlreadyPickedToday', (req, res) => {
+  User.find({ username: req.query.name, Date: req.query.date }, (err, response) => {
+    if (response.length > 0) {
+      res.send('found');
+    } else {
+      res.send('notfound');
     }
-  );
+  });
 });
 
-app.get("/games", (req, res) => {
+app.get('/games', (req, res) => {
   // console.log(req.query.product);	  console.log(req.query);
   let date = req.query.product;
   gamesApi(date, date).then(data => res.send(data.data));
 });
 
-app.get("/results", (req, res) => {
+app.get('/results', (req, res) => {
   // console.log(req.query);
   var results = [];
   {
-    User.find(
-      { username: req.query.name, Date: req.query.date },
-      (err, response) => {
-        if (response.length > 0) {
-          response[0].picks.map(pick => {
-            // console.log(pick.gameId);
-            // console.log(pick.selection);
-            axios
-              .get("https://www.balldontlie.io/api/v1/games/" + pick.gameId)
-              .then(dat => {
-                let gameWinner;
-                let selectionTeam;
-                // console.log(dat.data.date);
-                var homeTeam = {
-                  team: dat.data.home_team.full_name,
-                  score: dat.data.home_team_score,
-                  gameResult: "",
-                  selected: false
-                };
+    User.find({ username: req.query.name, Date: req.query.date }, (err, response) => {
+      if (response.length > 0) {
+        response[0].picks.map(pick => {
+          // console.log(pick.gameId);
+          // console.log(pick.selection);
+          axios.get('https://www.balldontlie.io/api/v1/games/' + pick.gameId).then(dat => {
+            let gameWinner;
+            let selectionTeam;
+            // console.log(dat.data.date);
+            var homeTeam = {
+              team: dat.data.home_team.full_name,
+              score: dat.data.home_team_score,
+              gameResult: '',
+              selected: false
+            };
 
-                var visitorTeam = {
-                  team: dat.data.visitor_team.full_name,
-                  score: dat.data.visitor_team_score,
-                  gameResult: "",
-                  selected: false
-                };
+            var visitorTeam = {
+              team: dat.data.visitor_team.full_name,
+              score: dat.data.visitor_team_score,
+              gameResult: '',
+              selected: false
+            };
 
-                var didHomeTeamWin =
-                  homeTeam.score > visitorTeam.score ? true : false;
+            var didHomeTeamWin = homeTeam.score > visitorTeam.score ? true : false;
 
-                if (didHomeTeamWin) {
-                  gameWinner = dat.data.home_team.full_name;
-                } else {
-                  gameWinner = dat.data.visitor_team.full_name;
-                }
+            if (didHomeTeamWin) {
+              gameWinner = dat.data.home_team.full_name;
+            } else {
+              gameWinner = dat.data.visitor_team.full_name;
+            }
 
-                selectionTeam = pick.selection;
+            selectionTeam = pick.selection;
 
-                results.push({
-                  homeTeam: homeTeam,
-                  visitorTeam: visitorTeam,
-                  gameWinner,
-                  selectionTeam
-                });
+            results.push({
+              homeTeam: homeTeam,
+              visitorTeam: visitorTeam,
+              gameWinner,
+              selectionTeam
+            });
 
-                if (selectionTeam == gameWinner) {
-                  Scores.findOneAndUpdate(
-                    { name: req.query.name },
-                    { $inc: { score: 100 } }
-                  );
-                  console.log("updated point :D");
-                }
+            if (selectionTeam == gameWinner) {
+              Scores.findOneAndUpdate({ name: req.query.name }, { $inc: { score: 100 } });
+              console.log('updated point :D');
+            }
 
-                if (results.length == response[0].picks.length) {
-                  // console.log(results);
-                  res.send(results);
-                }
-              });
+            if (results.length == response[0].picks.length) {
+              // console.log(results);
+              res.send(results);
+            }
           });
-        } else {
-          console.log("not found");
-          res.send("not found any data for this date");
-        }
+        });
+      } else {
+        console.log('not found');
+        res.send('not found any data for this date');
       }
-    );
+    });
   }
 });
 
-app.get("/data", (req, res) => {
-  console.log("retrieving data");
+app.get('/data', (req, res) => {
+  console.log('retrieving data');
   let team_id = req.query.team_id;
   prevGameApi(team_id)
     .then(data => {
       var currIdx = 0;
-      while (
-        data.data.data[currIdx].status === "Final" &&
-        currIdx != data.data.data.length - 1
-      ) {
+      while (data.data.data[currIdx].status === 'Final' && currIdx != data.data.data.length - 1) {
         currIdx++;
       }
       if (currIdx != data.data.data.length - 1) {
@@ -202,7 +184,7 @@ app.get("/data", (req, res) => {
           .catch(err => {
             console.log(err);
           });
-      } else res.send("no current games");
+      } else res.send('no current games');
     })
     .catch(err => {
       console.log(err);
@@ -222,33 +204,23 @@ function compareByPoints(a, b) {
 
 // BallDontLie API calls
 async function gamesApi(start, end) {
-  return await axios.get(
-    "https://www.balldontlie.io/api/v1/games?start_date=" +
-      start +
-      "&end_date=" +
-      end
-  );
+  return await axios.get('https://www.balldontlie.io/api/v1/games?start_date=' + start + '&end_date=' + end);
 }
 
 async function prevGameApi(team_id) {
-  return await axios.get(
-    "https://www.balldontlie.io/api/v1/games?per_page=100&seasons[]=2018&team_ids[]=" +
-      team_id
-  );
+  return await axios.get('https://www.balldontlie.io/api/v1/games?per_page=100&seasons[]=2018&team_ids[]=' + team_id);
 }
 
 async function statsApi(team_id, game_id) {
-  return await axios.get(
-    "https://www.balldontlie.io/api/v1/stats?per_page=100&game_ids[]=" + game_id
-  );
+  return await axios.get('https://www.balldontlie.io/api/v1/stats?per_page=100&game_ids[]=' + game_id);
 }
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 app.listen(port, () => {
